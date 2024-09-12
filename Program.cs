31d6da6eb58fbec0cfe8cd076517ae3d2ec4770e
Kaue Pilger <kauesouzapilger@gmail.com>
2024-09-12 20:25:55 +0200

    :tada: Desafio entregue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
